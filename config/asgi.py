112ffe96d4866d919dd3c1dfe449761b781b5bd9
ichikawaarata <maroon5.gingerale@gmail.com>
2024-03-17 11:18:35 +0900

    いつの間にかバグっていたディレクトリ構成を修正

--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for api_training_share project.
+ASGI config for config project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_training_share.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_asgi_application()
