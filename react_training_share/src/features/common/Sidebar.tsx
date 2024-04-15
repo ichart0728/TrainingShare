cbd0f1ab66920c2defb02d39b7477e2aa02bc575
ichikawaarata <maroon5.gingerale@gmail.com>
2024-04-15 23:42:08 +0900

    サイドバーをレスポンシブルデザインに対応

--- react_training_share/src/features/common/Sidebar.tsx
+++ react_training_share/src/features/common/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../../app/store";
 import StyledBadge from "../home/StyledBadge";
@@ -16,7 +16,11 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
+  IconButton,
+  useMediaQuery,
+  useTheme,
 } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 import HomeIcon from "@material-ui/icons/Home";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import FitnessCenter from "@material-ui/icons/FitnessCenter";
@@ -28,89 +32,132 @@ const Sidebar = () => {
   const dispatch: AppDispatch = useDispatch();
   const myprofile = useSelector(selectMyProfile);
   const navigate = useNavigate();
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
   const handleHomeClick = () => {
     navigate("/");
+    setMobileOpen(false);
   };
 
   const handleMyProfileClick = () => {
     dispatch(setUserProfileId(myprofile.userProfile));
     navigate(`profile/${myprofile.nickName}`);
+    setMobileOpen(false);
   };
 
   const handleWorkoutClick = () => {
     navigate("/workout");
+    setMobileOpen(false);
   };
 
   const handleLogoutClick = () => {
     const confirmLogout = window.confirm("ログアウトしますか？");
     if (confirmLogout) {
-      // ログアウトしたらJWTトークンを削除
       localStorage.removeItem("localJWT");
-      // dispatchでstoreを初期化
       dispatch(editNickname(""));
       dispatch(resetOpenProfile());
       dispatch(resetOpenNewPost());
       dispatch(setOpenSignIn());
       navigate("/");
+      setMobileOpen(false);
     }
   };
+
   return (
-    <Drawer className={styles.drawer} variant="permanent" anchor="left">
-      <div className={styles.drawerHeader}>
-        <h1 className={styles.sidebar_title}>Training Share</h1>
+    <>
+      {/* <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        edge="start"
+        onClick={handleDrawerToggle}
+        className={styles.menuButton}
+      >
+        <MenuIcon className={styles.icon} />
+      </IconButton> */}
+      <div className={styles.menuButtonWrapper}>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          className={styles.menuButton}
+        >
+          <MenuIcon className={styles.icon} />
+        </IconButton>
       </div>
-      <List className={styles.drawerContent}>
-        <ListItem button key="Home" onClick={handleHomeClick}>
-          <ListItemIcon>
-            <div className={styles.iconWrapper}>
-              <HomeIcon className={(styles.icon, "icon")} />
-            </div>
-          </ListItemIcon>
-          <ListItemText primary="Home" />
-        </ListItem>
+      <Drawer
+        className={styles.drawer}
+        variant={isMobile ? "temporary" : "permanent"}
+        open={isMobile ? mobileOpen : true}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true,
+        }}
+        classes={{
+          paper: styles.drawerContent,
+        }}
+      >
+        <div className={styles.drawerHeader}>
+          <h1 className={styles.sidebar_title}>Training Share</h1>
+        </div>
+        <List className={styles.drawerContent}>
+          <ListItem button key="Home" onClick={handleHomeClick}>
+            <ListItemIcon>
+              <div className={styles.iconWrapper}>
+                <HomeIcon className={styles.icon} />
+              </div>
+            </ListItemIcon>
+            <ListItemText primary="Home" className={styles.listItemText} />
+          </ListItem>
 
-        <ListItem button key="Workout" onClick={handleWorkoutClick}>
-          <ListItemIcon>
-            <div className={styles.iconWrapper}>
-              <FitnessCenter className={(styles.icon, "icon")} />
-            </div>
-          </ListItemIcon>
-          <ListItemText primary="Workout" />
-        </ListItem>
+          <ListItem button key="Workout" onClick={handleWorkoutClick}>
+            <ListItemIcon>
+              <div className={styles.iconWrapper}>
+                <FitnessCenter className={styles.icon} />
+              </div>
+            </ListItemIcon>
+            <ListItemText primary="Workout" className={styles.listItemText} />{" "}
+          </ListItem>
 
-        <ListItem button key="Profile" onClick={handleMyProfileClick}>
-          <ListItemIcon>
-            <div className={styles.iconWrapper}>
-              <StyledBadge
-                overlap="circular"
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "right",
-                }}
-                variant="dot"
-              >
-                <Avatar
-                  className={styles.avatar}
-                  alt="who?"
-                  src={myprofile.img}
-                />
-              </StyledBadge>
-            </div>
-          </ListItemIcon>
-          <ListItemText primary="Profile" />
-        </ListItem>
+          <ListItem button key="Profile" onClick={handleMyProfileClick}>
+            <ListItemIcon>
+              <div className={styles.iconWrapper}>
+                <StyledBadge
+                  overlap="circular"
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right",
+                  }}
+                  variant="dot"
+                >
+                  <Avatar
+                    className={styles.avatar}
+                    alt="who?"
+                    src={myprofile.img}
+                  />
+                </StyledBadge>
+              </div>
+            </ListItemIcon>
+            <ListItemText primary="Profile" className={styles.listItemText} />
+          </ListItem>
 
-        <ListItem button key="Logout" onClick={handleLogoutClick}>
-          <ListItemIcon>
-            <div className={styles.iconWrapper}>
-              <ExitToAppIcon className={(styles.icon, "icon")} />
-            </div>
-          </ListItemIcon>
-          <ListItemText primary="Logout" />
-        </ListItem>
-      </List>
-    </Drawer>
+          <ListItem button key="Logout" onClick={handleLogoutClick}>
+            <ListItemIcon>
+              <div className={styles.iconWrapper}>
+                <ExitToAppIcon className={styles.icon} />
+              </div>
+            </ListItemIcon>
+            <ListItemText primary="Logout" className={styles.listItemText} />
+          </ListItem>
+        </List>
+      </Drawer>
+    </>
   );
 };
 
