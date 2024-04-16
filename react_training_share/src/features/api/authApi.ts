6424232015757bb65ed6b595843d572fb3dbef63
ichikawaarata <maroon5.gingerale@gmail.com>
2024-04-16 22:46:40 +0900

    APIを分離

---
+++ react_training_share/src/features/api/authApi.ts
@@ -0,0 +1,89 @@
+import axios from "axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { PROPS_AUTHEN, PROPS_PROFILE, PROPS_NICKNAME } from "../types";
+
+const apiUrl = process.env.REACT_APP_DEV_API_URL;
+
+/*JWTトークン取得*/
+export const fetchAsyncLogin = createAsyncThunk(
+  "auth/post",
+  async (authen: PROPS_AUTHEN) => {
+    const res = await axios.post(`${apiUrl}authen/jwt/create`, authen, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  }
+);
+
+/*ユーザー新規作成*/
+export const fetchAsyncRegister = createAsyncThunk(
+  "auth/register",
+  async (auth: PROPS_AUTHEN) => {
+    const res = await axios.post(`${apiUrl}api/register/`, auth, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  }
+);
+
+/*プロフィール新規作成*/
+export const fetchAsyncCreateProf = createAsyncThunk(
+  "profile/post",
+  async (nickName: PROPS_NICKNAME) => {
+    const res = await axios.post(`${apiUrl}api/profile/`, nickName, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+/*プロフィール更新*/
+export const fetchAsyncUpdateProf = createAsyncThunk(
+  "profile/put",
+  async (profile: PROPS_PROFILE) => {
+    const uploadData = new FormData();
+    uploadData.append("nickName", profile.nickName);
+    profile.img && uploadData.append("img", profile.img, profile.img.name);
+    const res = await axios.put(
+      `${apiUrl}api/profile/${profile.id}/`,
+      uploadData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      }
+    );
+    return res.data;
+  }
+);
+
+/*プロフィール取得*/
+export const fetchAsyncGetMyProf = createAsyncThunk(
+  "myprofile/get",
+  async () => {
+    const res = await axios.get(`${apiUrl}api/myprofile/`, {
+      headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data[0];
+  }
+);
+
+/*プロフィール一覧取得*/
+export const fetchAsyncGetProfs = createAsyncThunk("profiles/get", async () => {
+  const res = await axios.get(`${apiUrl}api/profile/`, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data;
+});
---
+++ react_training_share/src/features/api/postApi.ts
@@ -0,0 +1,102 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+import axios from "axios";
+import { PROPS_NEWPOST, PROPS_LIKED, PROPS_COMMENT } from "../types";
+
+const apiUrlPost = `${process.env.REACT_APP_DEV_API_URL}api/post/`;
+const apiUrlComment = `${process.env.REACT_APP_DEV_API_URL}api/comment/`;
+
+/*投稿の取得*/
+export const fetchAsyncGetPosts = createAsyncThunk("post/get", async () => {
+  const res = await axios.get(apiUrlPost, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data;
+});
+
+/*新規投稿作成*/
+export const fetchAsyncNewPost = createAsyncThunk(
+  "post/post",
+  async (newPost: PROPS_NEWPOST) => {
+    const uploadData = new FormData();
+    uploadData.append("title", newPost.title);
+    newPost.img && uploadData.append("img", newPost.img, newPost.img.name);
+    const res = await axios.post(apiUrlPost, uploadData, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+/*投稿のいいねを更新*/
+export const fetchAsyncPatchLiked = createAsyncThunk(
+  "post/patch",
+  async (liked: PROPS_LIKED) => {
+    const currentLiked = liked.current;
+    const uploadData = new FormData();
+
+    /*いいねの取り消しを制御する*/
+    let isOverlapped = false;
+    currentLiked.forEach((current) => {
+      if (current === liked.new) {
+        isOverlapped = true;
+      } else {
+        uploadData.append("liked", String(current));
+      }
+    });
+
+    /*未いいねの場合は追加*/
+    if (!isOverlapped) {
+      uploadData.append("liked", String(liked.new));
+    } else if (currentLiked.length === 1) {
+      /*いいねが0件になる場合はpatchで更新できないので特別にputで空の配列で更新する*/
+      uploadData.append("title", liked.title);
+      const res = await axios.put(`${apiUrlPost}${liked.id}/`, uploadData, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      });
+      return res.data;
+    }
+    /*自分のいいねを解除した状態で更新*/
+    const res = await axios.patch(`${apiUrlPost}${liked.id}/`, uploadData, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+/*コメントの一覧を取得*/
+export const fetchAsyncGetComments = createAsyncThunk(
+  "comment/get",
+  async () => {
+    const res = await axios.get(apiUrlComment, {
+      headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+/*コメント新規作成*/
+export const fetchAsyncPostComment = createAsyncThunk(
+  "comment/post",
+  async (comment: PROPS_COMMENT) => {
+    const res = await axios.post(apiUrlComment, comment, {
+      headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
