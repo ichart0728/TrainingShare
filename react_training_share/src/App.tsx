cb98bf21e44c3734576ff226dad9dc3fb3f21497
ichikawaarata <maroon5.gingerale@gmail.com>
2024-04-18 23:54:36 +0900

    Sidebarファイルの配置先変更

--- react_training_share/src/App.tsx
+++ react_training_share/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "./app/store";
 
-import Sidebar from "./features/common/Sidebar";
+import Sidebar from "./features/components/Sidebar";
 import Home from "./features/home/Home";
 import Workout from "./features/workout/Workout";
 import Profile from "./features/profile/Profile";
