b217a0792fe12d8abdc4aaa52513ef8f68865b7b
ichikawaarata <maroon5.gingerale@gmail.com>
2024-04-25 00:22:39 +0900

    [WIP]グラフ CSS修正中

---
+++ react_training_share/src/features/components/graph/BodyPartChart.tsx
@@ -0,0 +1,146 @@
+// BodyPartChart.tsx
+
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+import { Tabs, Tab, useMediaQuery, useTheme } from "@material-ui/core";
+import styles from "./BodyPartChart.module.css";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  TimeScale,
+  Title,
+  Tooltip,
+} from "chart.js";
+import "chartjs-adapter-date-fns";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  TimeScale,
+  Title,
+  Tooltip
+);
+
+interface DataSet {
+  label: string;
+  data: { x: string; y: number }[];
+  borderColor: string;
+  fill: boolean;
+}
+
+interface ChartData {
+  labels: string[];
+  datasets: DataSet[];
+}
+
+interface WorkoutChartProps {
+  trainingSessions: any[];
+  selectedTab: number;
+  onTabChange: (event: ChangeEvent<{}>, newValue: number) => void;
+  trainingMenus: { id: number; name: string }[];
+}
+
+const bodyPartColors: { [key: number]: string } = {
+  1: "#FF6384",
+  2: "#36A2EB",
+  3: "#FFCE56",
+  4: "#4BC0C0",
+  5: "#9966FF",
+  6: "#FF9F40",
+};
+
+const WorkoutChart: React.FC<WorkoutChartProps> = ({
+  trainingSessions,
+  selectedTab,
+  onTabChange,
+  trainingMenus,
+}) => {
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: [],
+    datasets: [],
+  });
+
+  useEffect(() => {
+    const labels = trainingSessions.map(
+      (session) => new Date(session.date).toISOString().split("T")[0]
+    );
+    const datasets = trainingMenus.map((part) => ({
+      label: part.name,
+      data: trainingSessions.map((session) => ({
+        x: session.date,
+        y: session.workouts
+          .filter(
+            (workout: { body_part: string }) =>
+              Number(workout.body_part) === part.id
+          )
+          .reduce(
+            (acc: number, workout: { body_part: string; sets: any[] }) =>
+              acc +
+              workout.sets.reduce(
+                (accSet, set) => accSet + set.weight * set.reps,
+                0
+              ),
+            0
+          ),
+      })),
+      borderColor: bodyPartColors[part.id],
+      backgroundColor: bodyPartColors[part.id],
+      fill: false,
+    }));
+
+    setChartData({ labels, datasets });
+  }, [trainingSessions, trainingMenus]);
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  return (
+    <div className={styles.container}>
+      {/* <div className={styles.tabsContainer}> */}
+      <Tabs
+        value={selectedTab}
+        onChange={onTabChange}
+        aria-label="body part tabs"
+        variant={isMobile ? "scrollable" : "standard"}
+        scrollButtons={isMobile ? "on" : "auto"}
+        className={styles.tabsContainer}
+      >
+        <Tab label="すべて" className={styles.tab} />
+        {trainingMenus.map((part) => (
+          <Tab label={part.name} key={part.id} className={styles.tab} />
+        ))}
+      </Tabs>
+      {/* </div> */}
+      <div className={styles.chartContainer}>
+        {/* <Line
+          data={{
+            labels: chartData.labels,
+            datasets:
+              selectedTab === 0
+                ? chartData.datasets
+                : [chartData.datasets[selectedTab - 1]],
+          }}
+          options={{
+            scales: {
+              x: {
+                type: "time",
+                time: { unit: "day", displayFormats: { day: "yyyy-MM-dd" } },
+              },
+              y: { beginAtZero: true },
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+          }}
+          className={styles.chart}
+        /> */}
+      </div>
+    </div>
+  );
+};
+
+export default WorkoutChart;
--- react_training_share/src/features/workout_history/WorkoutHistory.tsx
+++ react_training_share/src/features/workout_history/WorkoutHistory.tsx
@@ -1,55 +1,12 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Line } from "react-chartjs-2";
-import { Tabs, Tab } from "@material-ui/core";
-import { fetchAsyncGetTrainingSessions } from "../api/workoutApi";
+// WorkoutHistory.tsx
+import React, { ChangeEvent, useState } from "react";
+import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
-import styles from "./WorkoutHistory.module.css";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  TimeScale,
-  Title,
-  Tooltip,
-} from "chart.js";
 import "chartjs-adapter-date-fns";
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  TimeScale,
-  Title,
-  Tooltip
-);
-
-const bodyPartColors: { [key: number]: string } = {
-  1: "#FF6384",
-  2: "#36A2EB",
-  3: "#FFCE56",
-  4: "#4BC0C0",
-  5: "#9966FF",
-  6: "#FF9F40",
-};
-
-interface DataSet {
-  label: string;
-  data: { x: string; y: number }[];
-  borderColor: string;
-  fill: boolean;
-}
-
-interface ChartData {
-  labels: string[];
-  datasets: DataSet[];
-}
+import BodyPartChart from "../components/graph/BodyPartChart";
+import styles from "./WorkoutHistory.module.css"; // CSSをインポート
 
 const WorkoutHistory = () => {
-  const dispatch = useDispatch();
   const trainingSessions = useSelector(
     (state: RootState) => state.workoutHistory.trainingSessions
   );
@@ -57,81 +14,19 @@ const WorkoutHistory = () => {
     (state: RootState) => state.training.trainingMenus
   );
   const [selectedTab, setSelectedTab] = useState(0);
-  const [chartData, setChartData] = useState<ChartData>({
-    labels: [],
-    datasets: [],
-  });
-
-  useEffect(() => {
-    dispatch(fetchAsyncGetTrainingSessions() as any);
-  }, [dispatch]);
-
-  useEffect(() => {
-    const labels = trainingSessions.map(
-      (session) => new Date(session.date).toISOString().split("T")[0]
-    );
-    const datasets = trainingMenus.map((part) => ({
-      label: part.name,
-      data: trainingSessions.map((session) => ({
-        x: session.date,
-        y: session.workouts
-          .filter((workout) => Number(workout.body_part) === part.id)
-          .reduce(
-            (acc, workout) =>
-              acc +
-              workout.sets.reduce(
-                (accSet, set) => accSet + set.weight * set.reps,
-                0
-              ),
-            0
-          ),
-      })),
-      borderColor: bodyPartColors[part.id],
-      backgroundColor: bodyPartColors[part.id],
-      fill: false,
-    }));
-
-    setChartData({ labels, datasets });
-  }, [trainingSessions, trainingMenus]);
-
   const handleTabChange = (event: ChangeEvent<{}>, newValue: number) => {
     setSelectedTab(newValue);
   };
 
   return (
-    <div>
-      <Tabs
-        value={selectedTab}
-        onChange={handleTabChange}
-        aria-label="body part tabs"
-      >
-        <Tab label="すべて" />
-        {trainingMenus.map((part, index) => (
-          <Tab label={part.name} key={part.id} />
-        ))}
-      </Tabs>
-      <div className={styles.chartContainer}>
-        <Line
-          data={{
-            labels: chartData.labels,
-            datasets:
-              selectedTab === 0
-                ? chartData.datasets
-                : [chartData.datasets[selectedTab - 1]],
-          }}
-          options={{
-            scales: {
-              x: {
-                type: "time",
-                time: { unit: "day", displayFormats: { day: "yyyy-MM-dd" } },
-              },
-              y: { beginAtZero: true },
-            },
-            responsive: true,
-            maintainAspectRatio: false,
-          }}
-        />
-      </div>
+    <div className={styles.historyContainer}>
+      {" "}
+      <BodyPartChart
+        trainingSessions={trainingSessions}
+        selectedTab={selectedTab}
+        onTabChange={handleTabChange}
+        trainingMenus={trainingMenus}
+      />
     </div>
   );
 };
