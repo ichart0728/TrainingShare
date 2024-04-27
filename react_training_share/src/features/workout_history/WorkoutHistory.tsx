fdff49522c329ed2b29f1050407ba75ac5dd53e9
ichikawaarata <maroon5.gingerale@gmail.com>
2024-04-27 19:36:09 +0900

    グラフ画面を表示した時に、過去のトレーニングセッションを取得するような処理を追加

--- react_training_share/src/features/workout_history/WorkoutHistory.tsx
+++ react_training_share/src/features/workout_history/WorkoutHistory.tsx
@@ -1,12 +1,20 @@
 // WorkoutHistory.tsx
-import React, { ChangeEvent, useState } from "react";
-import { useSelector } from "react-redux";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import "chartjs-adapter-date-fns";
 import BodyPartChart from "../components/graph/BodyPartChart";
-import styles from "./WorkoutHistory.module.css"; // CSSをインポート
+import styles from "./WorkoutHistory.module.css";
+import { fetchAsyncGetTrainingSessions } from "../api/workoutApi";
+import { AppDispatch } from "../../app/store";
 
 const WorkoutHistory = () => {
+  const dispatch: AppDispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAsyncGetTrainingSessions());
+  }, [dispatch]);
+
   const trainingSessions = useSelector(
     (state: RootState) => state.workoutHistory.trainingSessions
   );
